ae2652237cc33e6a1e96fe095b8e4df97ea42975
Ana Cunha <1771610+anacunha@users.noreply.github.com>
2024-11-14 08:02:14 -0600

    Create dummy Lambda function

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -1,9 +1,10 @@
 import { type ClientSchema, a, defineData } from '@aws-amplify/backend';
+import { sayHello } from '../functions/say-hello/resource';
 
 /*== STEP 1 ===============================================================
 The section below creates a Todo database table with a "content" field. Try
 adding a new "isDone" field as a boolean. The authorization rule below
-specifies that any unauthenticated user can "create", "read", "update", 
+specifies that any unauthenticated user can "create", "read", "update",
 and "delete" any "Todo" records.
 =========================================================================*/
 const schema = a.schema({
@@ -12,6 +13,15 @@ const schema = a.schema({
       content: a.string(),
     })
     .authorization((allow) => [allow.guest()]),
+
+  sayHello: a
+    .query()
+    .arguments({
+      name: a.string()
+    })
+    .returns(a.string())
+    .handler(a.handler.function(sayHello))
+    .authorization(allow => [allow.guest()]),
 });
 
 export type Schema = ClientSchema<typeof schema>;
@@ -28,7 +38,7 @@ Go to your frontend source code. From your client-side code, generate a
 Data client to make CRUDL requests to your table. (THIS SNIPPET WILL ONLY
 WORK IN THE FRONTEND CODE FILE.)
 
-Using JavaScript or Next.js React Server Components, Middleware, Server 
+Using JavaScript or Next.js React Server Components, Middleware, Server
 Actions or Pages Router? Review how to generate Data clients for those use
 cases: https://docs.amplify.aws/gen2/build-a-backend/data/connect-to-API/
 =========================================================================*/
--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,31 @@
-import './App.css'
+import './App.css';
+import { generateClient } from 'aws-amplify/api';
+import type { Schema } from '../amplify/data/resource';
+import { useState } from 'react';
+
+const client = generateClient<Schema>();
 
 function App() {
+  const [greeting, setGreeting] = useState<string | null>(null);
+
+  const sayHello = async () => {
+    const { data, errors } = await client.queries.sayHello({
+      name: window.prompt('What is your name?'),
+    });
+
+    if (!errors) {
+      setGreeting(data);
+    } else {
+      console.log(errors);
+    }
+  }
 
   return (
-    <>
+    <div>
       <h1>Event</h1>
-    </>
+      <button onClick={sayHello}>Say Hello</button>
+      <div>{ greeting }</div>
+    </div>
   )
 }
 
