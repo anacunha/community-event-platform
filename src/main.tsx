46d3949b58d4db7ee146e03575a03b1a06e7381e
Ana Cunha <1771610+anacunha@users.noreply.github.com>
2024-11-14 04:10:22 -0600

    Revert "Configure Authentication UI"
    This reverts commit eea29cb664dd9dca8af5d2caf59732452111d45c.

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,14 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import './index.css';
-import App from './App.tsx';
-import { Amplify } from "aws-amplify";
-import { Authenticator } from '@aws-amplify/ui-react';
-import outputs from "../amplify_outputs.json";
-import '@aws-amplify/ui-react/styles.css';
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.tsx'
+import { Amplify } from "aws-amplify"
+import outputs from "../amplify_outputs.json"
 
 Amplify.configure(outputs)
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Authenticator>
-      <App />
-    </Authenticator>
+    <App />
   </StrictMode>,
 )
