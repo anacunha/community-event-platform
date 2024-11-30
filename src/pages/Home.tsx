bb35e5b08d7eb0f190f1db8411cb68a30593dd74
Ana Cunha <1771610+anacunha@users.noreply.github.com>
2024-11-30 04:20:31 -0600

    Customize sign up form

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -22,8 +22,43 @@ function App() {
     }
   }
 
+  const formFields = {
+    signUp: {
+      family_name: {
+        placeholder: '',
+        isRequired: true,
+        label: 'First Name',
+        order: 1,
+      },
+      given_name: {
+        placeholder: '',
+        isRequired: true,
+        label: 'Last Name',
+        order: 2,
+      },
+      email: {
+        placeholder: '',
+        isRequired: true,
+        label: 'Email',
+        order: 3,
+      },
+      password: {
+        placeholder: '',
+        isRequired: true,
+        label: 'Password',
+        order: 4,
+      },
+      confirm_password: {
+        placeholder: '',
+        isRequired: true,
+        label: 'Confirm password',
+        order: 5,
+      },
+    },
+  }
+
   return (
-    <Authenticator>
+    <Authenticator formFields={formFields}>
       {({ signOut, user }) => (
         <main>
           <h1>{user?.signInDetails?.loginId}</h1>
