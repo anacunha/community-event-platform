f843d68b205cc4dc3787279741d933a2a574af18
Ana Cunha <1771610+anacunha@users.noreply.github.com>
2024-12-01 17:50:16 -0800

    Login/Sign Up page styling

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,7 @@
 import { generateClient } from 'aws-amplify/api';
 import type { Schema } from '../../amplify/data/resource';
 import { useState } from 'react';
-import { Authenticator } from '@aws-amplify/ui-react';
+import { Authenticator, Flex, Heading, Image } from '@aws-amplify/ui-react';
 
 const client = generateClient<Schema>({
   authMode: 'userPool',
@@ -68,16 +68,46 @@ function App() {
   }
 
   return (
-    <Authenticator formFields={formFields}>
-      {({ signOut, user }) => (
-        <main>
-          <h1>{user?.signInDetails?.loginId}</h1>
-          <button onClick={signOut}>Sign out</button>
-          <button onClick={sayHello}>Say Hello</button>
-          <div>{greeting}</div>
-        </main>
-      )}
-    </Authenticator>
+    <Flex>
+      <Flex
+        justifyContent="center"
+        direction="column"
+        alignItems="center"
+      >
+        <Heading
+          level={3}
+        >
+          Welcome to AWS Community Talkine!
+        </Heading>
+        <Authenticator formFields={formFields}>
+          {({ signOut, user }) => (
+            <main>
+              <h1>{user?.signInDetails?.loginId}</h1>
+              <button onClick={signOut}>Sign out</button>
+              <button onClick={sayHello}>Say Hello</button>
+              <div>{greeting}</div>
+            </main>
+          )}
+        </Authenticator>
+      </Flex>
+      <Flex
+        justifyContent="center"
+        alignItems="center"
+        backgroundColor="rgba(199,187,235,1)"
+        direction="column"
+      >
+        <Heading
+          color="rgba(255,255,255,1)"
+          level={3}
+        >
+          Here, you can submit your ideas and share your experiences!
+        </Heading>
+        <Image
+          src="/community.png"
+          alt="Illustration of a diverse, inclusive group of smiling people representing members of the AWS community."
+        />
+      </Flex>
+    </Flex>
   )
 }
 
