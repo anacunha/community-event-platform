c3d301915df6782067bad3939aedb490ac58bb5c
Ana Cunha <1771610+anacunha@users.noreply.github.com>
2024-12-03 08:22:07 -0800

    Update Home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,22 @@
+import { useEffect, useState } from "react";
+import type { Schema } from "../../amplify/data/resource";
+import { generateClient } from "aws-amplify/data";
+import Header from "../components/Header";
 import { Authenticator, Flex, Heading, Image, useTheme, View } from '@aws-amplify/ui-react';
 
+const client = generateClient<Schema>({
+  authMode: 'userPool',
+});
+
 function Home() {
+  const [sessions, setSessions] = useState<Array<Schema["Session"]["type"]>>([]);
+
+  useEffect(() => {
+    client.models.Session.observeQuery().subscribe({
+      next: (data) => setSessions([...data.items]),
+    });
+  }, []);
+
   const { tokens } = useTheme();
 
   const formFields = {
@@ -52,40 +68,60 @@ function Home() {
     Header() {
       return (
         <>
-          <Heading
-            level={1}
-            textAlign="center"
+          <Flex
+            justifyContent="center"
+            alignItems="center"
+            backgroundColor="rgba(199,187,235,1)"
+            style={{ minHeight: '20vh', paddingTop: '20px', paddingLeft: '20px', paddingRight: '20px' }}
+            direction="column"
           >
-            Welcome to AWS Community Talks!
-          </Heading>
-          <View textAlign="center" padding={tokens.space.large}>
-            <Image
-              alt="AWS Community members"
-              src="/community.png"
-              height="270px"
-            />
-          </View>
+            <Heading
+              level={1}
+              textAlign="center"
+
+            >
+              Welcome to AWS Community Talks!
+            </Heading>
+            <View textAlign="center" padding={tokens.space.large}>
+              <Image
+                alt="AWS Community members"
+                src="/community.png"
+                height="270px"
+              />
+            </View>
+          </Flex>
         </>
       );
     },
+
+    Footer() {
+      return (
+        <Flex
+          justifyContent="center"
+          alignItems="center"
+          backgroundColor="rgba(199,187,235,1)"
+          style={{ minHeight: '20vh', paddingTop: '20px', paddingLeft: '20px', paddingRight: '20px' }}
+          direction="column"
+        >
+        </Flex>
+      )
+    }
   }
 
   return (
-    <Flex
-      justifyContent="center"
-      alignItems="center"
-      backgroundColor="rgba(199,187,235,1)"
-      style={{ minHeight: '100vh' }}
-    >
-      <Authenticator formFields={formFields} components={components}>
-        {({ signOut, user }) => (
-          <main>
-            <h1>{user?.signInDetails?.loginId}</h1>
-            <button onClick={signOut}>Sign out</button>
-          </main>
-        )}
-      </Authenticator>
-    </Flex>
+    <Authenticator formFields={formFields} components={components}>
+      {({ signOut }) => (
+        <main>
+          <Header signOut={signOut} />
+          <h1>My Sessions</h1>
+          <ul>
+            {sessions.map((session) => (
+              <li key={session.id}>{session.title}</li>
+            ))}
+          </ul>
+        </main>
+      )}
+    </Authenticator>
   )
 }
 
